b53f22647b24cfb8e3ab76a6c5421174227c99a7
Alexandra Romanova <50080060+Aleksa45@users.noreply.github.com>
2021-04-29 12:39:21 +0300

    Add files via upload

---
+++ Invisible/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Invisible
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.userNameLabel = new System.Windows.Forms.Label();
+            this.processFocusNameLabel = new System.Windows.Forms.Label();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.errorBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Invisible";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // userNameLabel
+            // 
+            this.userNameLabel.AutoSize = true;
+            this.userNameLabel.Location = new System.Drawing.Point(213, 59);
+            this.userNameLabel.Name = "userNameLabel";
+            this.userNameLabel.Size = new System.Drawing.Size(46, 17);
+            this.userNameLabel.TabIndex = 0;
+            this.userNameLabel.Text = "label1";
+            // 
+            // processFocusNameLabel
+            // 
+            this.processFocusNameLabel.AutoSize = true;
+            this.processFocusNameLabel.Location = new System.Drawing.Point(213, 87);
+            this.processFocusNameLabel.Name = "processFocusNameLabel";
+            this.processFocusNameLabel.Size = new System.Drawing.Size(46, 17);
+            this.processFocusNameLabel.TabIndex = 1;
+            this.processFocusNameLabel.Text = "label1";
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(105, 20);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(251, 17);
+            this.errorLabel.TabIndex = 2;
+            this.errorLabel.Text = "Отсутствует подключение к серверу";
+            this.errorLabel.Visible = false;
+            // 
+            // errorBtn
+            // 
+            this.errorBtn.Location = new System.Drawing.Point(362, 12);
+            this.errorBtn.Name = "errorBtn";
+            this.errorBtn.Size = new System.Drawing.Size(126, 32);
+            this.errorBtn.TabIndex = 3;
+            this.errorBtn.Text = "Подключиться";
+            this.errorBtn.UseVisualStyleBackColor = true;
+            this.errorBtn.Visible = false;
+            this.errorBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cостояние: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Имя пользователя:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Текущий активный процесс:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 124);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.errorBtn);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.processFocusNameLabel);
+            this.Controls.Add(this.userNameLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Invoke";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label userNameLabel;
+        private System.Windows.Forms.Label processFocusNameLabel;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.Button errorBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Invisible/Form1.cs
@@ -0,0 +1,209 @@
+﻿using Server;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Invisible
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern Int32 GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern UInt32 GetWindowThreadProcessId(Int32 hWnd, out Int32 lpdwProcessId);
+
+        const string ERROR_TEXT = "Отсутствует подключение к серверу";
+        const string OK_TEXT = "Соединение установлено";
+
+        public static Thread lookAtThread;
+        public static string nameComp = "";
+
+        public static string address = "127.0.0.1";
+        public const int port = 8888;
+
+        public static TcpClient client;
+        public Form1()
+        {
+            InitializeComponent();
+            //получение имени пользователя
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT UserName FROM Win32_ComputerSystem");
+            ManagementObjectCollection collection = searcher.Get();
+            nameComp = (string)collection.Cast<ManagementBaseObject>().First()["UserName"];
+            nameComp = nameComp.Substring(nameComp.IndexOf("\\") + 1);
+            userNameLabel.Text = nameComp;
+
+            //создание и запуск процесса-шпиона
+            lookAtThread = new Thread(new ThreadStart(() =>
+            {
+                while (true)
+                {
+                    Int32 handle = GetForegroundWindow();
+                    Int32 ProcessID;
+                    GetWindowThreadProcessId(handle, out ProcessID);
+                    Process ActiveProcess = Process.GetProcessById(ProcessID);
+
+                    if (!ActiveProcess.ProcessName.Equals(processFocusNameLabel.Text) && this.InvokeRequired)
+                        this.Invoke(new Action(() => { OnChangeProcess?.Invoke(ActiveProcess.ProcessName); }));
+                    Thread.Sleep(1000);
+                }
+            }));
+            lookAtThread.Start();
+
+            //регистрация обработчика на смену процесса
+            OnChangeProcess += OnChangeProcessHandler;
+
+            //Создание и открытие соединения с сервером
+            ToConnectServer();
+        }
+
+
+
+
+        #region Logic
+        private void OnChangeProcessHandler(string processName)
+        {
+            processFocusNameLabel.Text = processName;
+
+            try
+            {
+                if (client != null)
+                {
+                    NetworkStream stream = client.GetStream();
+
+                    Package package = new Package()
+                    {
+                        Name = nameComp,
+                        DateTime = DateTime.Now,
+                        Operation = Operation.ChangeProcess,
+                        Data = processName
+                    };
+
+                    // преобразуем сообщение в массив байтов
+                    byte[] data = Encoding.Unicode.GetBytes(package.ToJson());
+
+                    // отправка сообщения
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage();
+            }
+
+        }
+
+        private void ShowErrorMessage()
+        {
+            //TODO
+            MessageBox.Show("Отсутствует соединение с сервером", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Show();
+            WindowState = FormWindowState.Normal;
+
+            errorLabel.Visible = true;
+            errorLabel.Text = ERROR_TEXT;
+            errorLabel.ForeColor = Color.Red;
+            errorBtn.Visible = true;
+
+            if (client != null)
+            {
+                client.Close();
+                client = null;
+            }
+
+        }
+        private void ToConnectServer()
+        {
+            client = null;
+            try
+            {
+                client = new TcpClient(address, port);
+                NetworkStream stream = client.GetStream();
+
+                Package package = new Package() { Name = nameComp, DateTime = DateTime.Now, Operation = Operation.Connect, Data = null };
+                // преобразуем сообщение в массив байтов
+                byte[] data = Encoding.Unicode.GetBytes(package.ToJson());
+                // отправка сообщения
+                stream.Write(data, 0, data.Length);
+                errorLabel.Visible = true;
+                errorLabel.Text = OK_TEXT;
+                errorLabel.ForeColor = Color.Green;
+                errorBtn.Visible = false;
+
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage();
+            }
+        }
+        #endregion
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+
+                Hide();
+                notifyIcon1.BalloonTipTitle = "Программа была спрятана";
+                notifyIcon1.BalloonTipText = "Обратите внимание что программа была спрятана в трей и продолжит свою работу.";
+                notifyIcon1.ShowBalloonTip(2000); // Параметром указываем количество миллисекунд, которое будет показываться 
+            }
+        }
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            lookAtThread.Abort();
+            if (client != null)
+            {
+                try
+                {
+                    NetworkStream stream = client.GetStream();
+
+                    Package package = new Package() { Name = nameComp, DateTime = DateTime.Now, Operation = Operation.Exit, Data = null };
+                    // преобразуем сообщение в массив байтов
+                    byte[] data = Encoding.Unicode.GetBytes(package.ToJson());
+                    // отправка сообщения
+                    stream.Write(data, 0, data.Length);
+
+
+                }
+                catch (Exception ex)
+                {
+
+                    if (client != null)
+                    {
+                        if (client.Connected)
+                            if (client.GetStream() != null)
+                                client.GetStream().Close();
+                        client.Close();
+                    }
+                }
+            }
+        }
+
+
+        delegate void ChangeProcess(string processName);
+        event ChangeProcess OnChangeProcess;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ToConnectServer();
+        }
+    }
+}
